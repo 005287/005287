ff618fecf7b4f49202838e721ea1f82cd4794a0e
005287 <90329282+005287@users.noreply.github.com>
2021-09-08 17:23:56 +0300

    Update Data Analysis Courses.sql

--- Data Analysis Courses.sql
+++ Data Analysis Courses.sql
@@ -5,7 +5,10 @@ FROM courses
 
 # information regarding each course - price, number of studetns and duration
 
-SELECT course_id, price_usd, num_students, content_duration_minutes
+SELECT course_id,
+       price_usd, 
+       num_students,
+       content_duration_minutes
 FROM courses
 
 # course title, num of students total reviews and ratio between reiews and students
@@ -19,19 +22,19 @@ FROM courses
 # course details and avg lesson duration
 
 SELECT course_id,
-course_title,
-num_lessons,
-content_duration_minutes,
-content_duration_minutes / num_lessons AS "avg_duration"
+       course_title,
+       num_lessons,
+       content_duration_minutes,
+       content_duration_minutes / num_lessons AS "avg_duration"
 FROM courses
 
 # course enrolment details and total revenue per course
 
 SELECT course_id,
-course_title,
-num_students,
-price_usd,
-num_students * price_usd AS "total_course_revenues"
+       course_title,
+       num_students,
+       price_usd,
+       num_students * price_usd AS "total_course_revenues"
 FROM courses
 
 # courses by title and subject
@@ -42,19 +45,19 @@ FROM courses
 
 # course name and id, price and duration. Decending by price
 SELECT course_title,
-course_id,
-price_usd,
-content_duration_minutes,
-content_duration_minutes / 60 AS "content_duration_hours"
+       course_id,
+       price_usd,
+       content_duration_minutes,
+       content_duration_minutes / 60 AS "content_duration_hours"
 FROM courses
 ORDER BY price_usd DESC
 
 # course name and id, price and duration. Decending by duration
 SELECT course_title,
-course_id,
-price_usd,
-content_duration_minutes,
-content_duration_minutes / 60 AS "content_duration_hours"
+       course_id,
+       price_usd,
+       content_duration_minutes,
+       content_duration_minutes / 60 AS "content_duration_hours"
 FROM courses
 ORDER BY content_duration_minutes DESC
 
@@ -76,8 +79,10 @@ FROM courses
 
 # num of course levels by subject - top 10
 
-SELECT course_subject, course_level, COUNT(course_level) AS 'num_of_courses'
+SELECT course_subject, 
+       course_level, 
+       COUNT(course_level) AS 'num_of_courses'
 FROM courses
-GROUP BY (CONCAT(course_subject,", ", course_level))
+GROUP BY (CONCAT(course_subject, course_level))
 ORDER BY num_of_courses DESC
 LIMIT 10
